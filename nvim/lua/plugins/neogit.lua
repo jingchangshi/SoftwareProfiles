e608630e1cfdab4c40ddc5d2f60105e69e08895e
shijingchang <shijingchang@huawei.com>
2026-04-29 16:04:31 +0000

    feat: nvim add neogit

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,62 @@
+return {
+  "NeogitOrg/neogit",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "sindrets/diffview.nvim",
+  },
+  opts = {
+    disable_hint = false,
+    disable_context_highlighting = false,
+    disable_signs = false,
+    disable_insert_on_commit = "auto",
+    filewatcher = {
+      interval = 1000,
+      enabled = true,
+    },
+    graph_style = "ascii",
+    remember_settings = true,
+    use_per_project_settings = true,
+    highlight = {
+      italic = true,
+      bold = true,
+      underline = true,
+    },
+    use_default_keymaps = true,
+    auto_refresh = true,
+    kind = "tab",
+    commit_popup = {
+      kind = "split",
+    },
+    preview_buffer = {
+      kind = "split",
+    },
+    popup = {
+      kind = "split",
+    },
+    sort_branches = "-committerdate",
+  },
+  config = function(_, opts)
+    local neogit = require("neogit")
+    neogit.setup(opts)
+
+    local function map(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { desc = desc })
+    end
+
+    map("n", "<leader>gg", neogit.open, "打开 Neogit")
+    map("n", "<leader>gc", function()
+      neogit.open({ "commit" })
+    end, "打开 Commit 窗口")
+    map("n", "<leader>gp", function()
+      neogit.open({ "pull" })
+    end, "打开 Pull 窗口")
+    map("n", "<leader>gP", function()
+      neogit.open({ "push" })
+    end, "打开 Push 窗口")
+    map("n", "<leader>gb", function()
+      neogit.open({ "branch" })
+    end, "打开 Branch 窗口")
+  end,
+}
